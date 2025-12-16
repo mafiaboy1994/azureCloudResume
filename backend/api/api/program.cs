e71c0daacc2fdcbf0df37f0c179b464b96c99516
Elijah Wright <ebwright@protonmail.com>
2025-12-16 20:21:12 +0000

    updated some files

--- backend/api/api/program.cs
+++ backend/api/api/program.cs
@@ -1,5 +1,6 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
--- obj/Debug/net6.0/azureCloudResume.AssemblyInfo.cs
+++ obj/Debug/net6.0/azureCloudResume.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("azureCloudResume")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e356d809bce9a965174c8335bc71bb51739c687a")]
 [assembly: System.Reflection.AssemblyProductAttribute("azureCloudResume")]
 [assembly: System.Reflection.AssemblyTitleAttribute("azureCloudResume")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
